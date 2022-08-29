6e23779f91b86c9aa07be258162efbe709e54726
Heber Vela <vela.heber29@gmail.com>
2022-08-29 02:38:37 -0300

    Added Navbar

--- src/components/NavBar.js
+++
@@ -1,9 +0,0 @@
-const NavBar = () => {
-    return (
-        <div>
-            Hola
-        </div>
-    )
-}
-
-export default NavBar
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <div className="header-style">
+            <h2 className="logo">VyR Tecnología</h2>
+
+            <ul className="nav-list">
+                <a href="" target="_blank" className="nav-item">Inicio</a>
+                <a href="" target="_blank" className="nav-item">Productos</a>
+                <a href="" target="_blank" className="nav-item">Contacto</a>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
